012449601ddcf291a79f9f1df9f37730818b8814
NaveenAdmin-at-623916870671 <nmunibac@amazon.com>
2020-08-06 17:51:56 -0500

    Source files

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		return "Checking the fargate logging with Java application";
 	}
 
 	public static void main(String[] args) {

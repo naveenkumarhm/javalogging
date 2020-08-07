af578408b3765116b34fff9165d54b4294a05abb
NaveenAdmin-at-623916870671 <nmunibac@amazon.com>
2020-08-06 21:54:57 -0500

    test

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.log4j.Log4j2;
 
 @SpringBootApplication
 @RestController
+@Log4j2
 public class Application {
 
 	@RequestMapping("/")

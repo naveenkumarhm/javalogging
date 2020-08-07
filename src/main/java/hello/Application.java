7f593d11f4490706111c46da52ec3b8404c6e0d9
NaveenAdmin-at-623916870671 <nmunibac@amazon.com>
2020-08-06 22:19:07 -0500

    tsting

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -15,8 +15,11 @@ public class Application {
 	public String home() {
 		
 		
-		log.trace("A TRACE Message");
-        log.debug("A DEBUG Message");
+		for(int i=0; i<= 10000; i++) {
+			//some business logic
+			//In each iteration we are printing single line logger anf this is causing problem
+			log.info("TREPP Analytics Date is NULL for Instrument[instrumentId: " + 1122334454 + "] ");
+			}
         log.info("An INFO Message");
         log.warn("A WARN Message");
         log.error("An ERROR Message");
